0e8be82c6b16272140e33259237f2ac5403ea89d
Goran <gvesenjak@gmail.com>
2013-09-16 02:01:36 +0200

    Created a rails application and added status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fejsbook::Application.config.secret_token = '22e0db0018405b15fccfb04ef5f7cc40509c831847ba1fbdf7f3c2a194dc3971825230e4959e927e537d77c1c392d475363a7209df71355ab4729a34f4f654c5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Fejsbook::Application.config.session_store :cookie_store, key: '_Fejsbook_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Fejsbook::Application.config.session_store :active_record_store
